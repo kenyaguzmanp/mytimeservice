bfb126374d9d0865811e1bc9e72d1eb9b167588d
Reny Alvarado <renyalvarado@gmail.com>
2017-08-15 19:40:43 -0300

    added the unixToNatural function

--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,24 @@ router.get('/', function(req, res, next) {
 /*Creating a new route to the JSON */
 //time is a parameter
  router.get('/:time', function(req, res){
-   var data = { time: req.params.time };
-   res.json(data);
+  function unixToNatural(unix){
+    var date = new Date(unix*1000);
+    var months = ['January', 'February','March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var month = months[date.getMonth()];
+    var day = date.getDay();
+    var year = date.getFullYear();
+
+    var result = month + ' ' + day + ', ' + year;
+    return result;
+
+  }
+
+   if(!isNaN(req.params.time)){
+     var result = unixToNatural(req.params.time);
+     var data = {"unix": req.params.time,  "natural": result};
+     res.json(data);
+
+   }
  }); 
 
 module.exports = router;
